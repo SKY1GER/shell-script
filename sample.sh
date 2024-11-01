7fedb0babd5a1b551e0d68768f3d75e7bc28c21c
SKY1GER <saikumarb9119@gmail.com>
2024-11-01 13:15:46 +0530

    updated

--- sample.sh
+++ sample.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-user_id=$(id -u)
-script_name=$(echo $0 | cut -d "." -f1)
-time_stamp=$(date +%F-%H-%M-%S)
+userid=$(id -u)
+scriptname=$(echo $0 | cut -d "." -f1)
+timestamp=$(date +%F-%H-%M-%S)
 logfile=/tmp/$script_name-$time_stamp.log
 R="\e[31m"
 G="\e[32m"
@@ -10,7 +10,7 @@ N="\e[0m"
 
 
 VALIDATE(){
-    if [$1 -ne 0]
+    if [ $1 -ne 0 ]
     then
       echo -e "$2 $R installation failed $N"
       exit 1
@@ -19,7 +19,7 @@ VALIDATE(){
     fi
 }
 
-if [$user_id -ne 0]
+if [ $userid -ne 0 ]
 then
     echo "please run this script in superuser"
     exit 1 #manually exit if error comes
@@ -31,12 +31,11 @@ for i in $@
 do
    echo "package to install:$i"
    dnf list installed $i &>>$logfile
-   if [$? -e 0]
+   if [ $? -e 0 ]
    then
       echo -e "$i $Y already installed $N"
     else
        dnf install $i -y &>>$logfile
        validate $? "$i installation"
     fi
-done    
-      
+done
